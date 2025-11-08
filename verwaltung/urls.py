ba3f52c495d83bdfcd825fe6993a2a6c2e8a2e14
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2025-11-08 21:29:28 +0100

    ajax

--- verwaltung/urls.py
+++ verwaltung/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.start, name="" )
+    path('', views.start, name="start" ),
+    path('login/', views.login, name="login" ),
+
 ]
--- verwaltung/views.py
+++ verwaltung/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 def start(request):
     return render(request, 'verwaltung/start.html')
+
+def login(request):
+    print("login")
+    return HttpResponse("Liked!")
