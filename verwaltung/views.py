ae8f8e1aff70f17678ffb4c367a5d881743cd5cd
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2025-11-09 12:07:20 +0100

    feedback login

--- verwaltung/views.py
+++ verwaltung/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 
@@ -12,12 +13,12 @@ def user_login(request):
     user = authenticate(request, username=name, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponse("Success")
+        return JsonResponse({'status': 'success'})
     # Redirect to a success page.
     else:
+        return JsonResponse({'status': 'error'})
         # Return an 'invalid login' error message.
-        message = "Eingabe inkorrekt"
-        return HttpResponse("Error")
+
 
 def user_logout(request):
     logout(request)
